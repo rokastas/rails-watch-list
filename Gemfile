ed983e423f40f0156fbbb7a4c83ff32b94ad1165
Rokas Stasiulis <rokastas@gmail.com>
2024-01-25 19:10:59 +0100

    site almost working, missing delete

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem "font-awesome-sass", "~> 6.1"
 gem "simple_form"
 gem "sassc-rails"
 
+gem 'faker', '~> 3.2'
+
 ruby "3.1.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
